df4c59ca7f4d7d9ca04c5875462a86597f657668
François Hodierne <francois@hodierne.net>
2020-09-23 08:41:31 +0200

    implement akismet based spam tracking

---
+++ scripts/check-conversation-spam.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+import '../server/env';
+
+import { AkismetClient } from 'akismet-api';
+import config from 'config';
+import { get } from 'lodash';
+
+import { sleep } from '../server/lib/utils';
+import models from '../server/models';
+
+const key = config.akismet.key;
+const blog = 'https://blog.opencollective.com';
+const client = new AkismetClient({ key, blog });
+
+async function run() {
+  const conversations = await models.Conversation.findAll({
+    limit: 100,
+    order: [['createdAt', 'DESC']],
+  });
+
+  for (const conversation of conversations) {
+    await sleep(Math.random() * 1000 * 5);
+    const user = await models.User.findByPk(conversation.CreatedByUserId);
+    const collective = await models.Collective.findByPk(conversation.FromCollectiveId);
+
+    const ip = get(user, 'data.lastSignInRequest.ip', get(user, 'data.creationRequest.ip'));
+    const useragent = get(user, 'data.lastSignInRequest.userAgent', get(user, 'data.creationRequest.userAgent'));
+
+    const comment = {
+      ip,
+      useragent,
+      content: `${conversation.title} ${conversation.summary}`,
+      email: user.email,
+      name: collective.name,
+      type: 'forum-post',
+    };
+
+    try {
+      const isSpam = await client.checkSpam(comment);
+
+      if (isSpam) {
+        console.log(conversation.id, conversation.title, { result: 'OMG Spam!' });
+        await conversation.update({ data: { ...conversation.data, spam: true } });
+      } else {
+        console.log(conversation.id, conversation.title, { result: 'Not spam' });
+        await conversation.update({ data: { ...conversation.data, spam: false } });
+      }
+    } catch (err) {
+      console.error('Something went wrong:', err.message);
+    }
+  }
+}
+
+run();
